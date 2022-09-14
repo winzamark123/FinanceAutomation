271f04d5c1f84855d50e5720283987a02eb986e5
Win Cheng <wincheng@Wins-MacBook-Pro.local>
2022-09-15 00:10:23 +0700

    Removed HashMap Class

--- convert_pdf.py
+++ convert_pdf.py
@@ -1,18 +1,20 @@
 import pdftables_api
 from scanner import *
 
+#Starting Directories
 dest_dir = "/Users/wincheng/Desktop/VSCoding/FinanceAutomation"
 
+#convert the file_name into CSV
 def convert_to_CSV(file_name):
     c = pdftables_api.Client('zu9hzrgc0b3u')
     c.csv(file_name, file_name) 
 #replace c.xlsx with c.csv to convert to CSV
-#replace c.xlsx with c.xml to convert to XML
-#replace c.xlsx with c.html to convert to HTML
 
+
+#============================================================
 for key, values in hash.items():
     for value in values:
-        #print(str(key) + ":" + str(value))
+        
         Copy_Files(str(key), str(value), dest_dir)
         print()
 
--- scanner.py
+++ scanner.py
@@ -30,6 +30,7 @@ def Copy_Files(source_dir, file_name, dest_dir):
         print(file_name + "is already in the folder")
         return
 
+#scanning the files within the dir
 def Scan_Files(source_dir):
 
     names_array = []
@@ -51,63 +52,6 @@ def Scan_Files(source_dir):
                 #Copy_Files(source_dir, file_name, dest_dir)
     
     return names_array
-
-class HashMap:
-    #initialize hashmap size 
-    def __init__(self):
-            self.size = 64
-            self.map = [None] * self.size
-    
-    #getting the hash index 
-    def _get_hash(self, key):
-            hash = 0
-            for char in str(key):
-                    hash += ord(char)
-            return hash % self.size
-    
-    #adding a key + value to the hashmap
-    def add(self, key, value):
-        key_hash = self._get_hash(key)
-        key_value = [key, value]
-
-        if self.map[key_hash] is None:
-                self.map[key_hash] = list([key_value])
-                return True
-        else:
-                for pair in self.map[key_hash]:
-                        if pair[0] == key:
-                                pair[1] = value
-                                return True
-                self.map[key_hash].append(key_value)
-                return True
-    
-    #getting the value using the key
-    def get(self, key):
-        key_hash = self._get_hash(key)
-        if self.map[key_hash] is not None:
-                for pair in self.map[key_hash]:
-                        if pair[0] == key:
-                                return pair[1]
-        return None
-    
-    #deleting the value using the key
-    def delete(self, key):
-        key_hash = self._get_hash(key)
-
-        if self.map[key_hash] is None:
-                return False
-        for i in range (0, len(self.map[key_hash])):
-                if self.map[key_hash][i][0] == key:
-                        self.map[key_hash].pop(i)
-                        return True
-        return False
-
-    #printing the entire hash list
-    def print(self):   
-        for item in self.map:
-                if item is not None:
-                        print(str(item))
-
    
 #============================================================
 #Starting Hash
@@ -127,8 +71,3 @@ with os.scandir(main_source_dir) as itr:
         hash[dir_name] = Scan_Files(dir_name)
          
     #print(hash)
-
-
-#while itr running, use Scan_Files again to go into specific directory 
-#add each files into the hash map
-#go through all the hashmaps and copy the files into Finance Automation dir using key and value

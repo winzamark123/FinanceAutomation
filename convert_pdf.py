a93ddedc7d94b7146913b1b45132511a39a2830e
Win Cheng <wincheng@Wins-MacBook-Pro.local>
2022-09-17 01:10:05 +0700

    Optimized the program so that it is easier to read / code out // Also added priv dirs for CSV and PDF Files for organization

--- convert_pdf.py
+++ convert_pdf.py
@@ -2,7 +2,8 @@ import pdftables_api
 from scanner import *
 
 #Starting Directories
-dest_dir = "/Users/wincheng/Desktop/VSCoding/FinanceAutomation"
+pdf_dest_dir = "/Users/wincheng/Desktop/VSCoding/FinanceAutomation/PDF_Copied"
+csv_dest_dir = "/Users/wincheng/Desktop/VSCoding/FinanceAutomation/CSV_Copied"
 
 #convert the file_name into CSV
 def convert_to_CSV(file_name):
@@ -10,18 +11,57 @@ def convert_to_CSV(file_name):
     c.csv(file_name, file_name) 
 #replace c.xlsx with c.csv to convert to CSV
 
+#moving the files in source_dir with dest_dir 
+def Move_Files(source_dir, file_name, dest_dir):
+    
+    files_exist = os.path.exists(dest_dir + "/" + file_name)
+
+    #if the file is already in the folder 
+    if files_exist:
+        print("The file is already in the folder")
+        return
+
+    #updates the src_path with the folder's name + the file name
+    src_path = source_dir + "/" + file_name
+
+    shutil.move(src_path, dest_dir)
+
 
 #============================================================
-for key, values in hash.items():
-    for value in values:
-        
-        Copy_Files(str(key), str(value), dest_dir)
-        print()
+#copying pdf files from the main dir (Finance_INFO) into dest_dir (PDF_Copied) 
+def copying_PDF_files():
+    for key, values in hash.items():
+        for value in values:
+            
+            Copy_Files(str(key), str(value), pdf_dest_dir)
+            
+    print("Finished Copying!")
+    print()
+  
+#moving the converted CSV files into CSV_Copied dir 
+def moving_CSV_files():
+    CSV_Files = Scan_Files(pdf_dest_dir, ".csv")
 
-files_in_dir = Scan_Files(dest_dir)
+    for csv_file in CSV_Files:
+        Move_Files(pdf_dest_dir, csv_file, csv_dest_dir)
 
-#for pdf_files in files_in_dir:
-   #convert_to_CSV(pdf_files)
+    print("Finished Moving!")
+    print()
 
-#Hashmap Structure:
-#key: source_dir, value: file_name
+
+#converting all pdf files in PDF_Copied to csv files
+def convert_all_to_CSV():
+    PDF_Files = Scan_Files(pdf_dest_dir, ".pdf")
+
+    for pdf_file in PDF_Files:
+        convert_to_CSV(pdf_file)
+
+    print("Finished Converting!")
+    print()
+
+print("Starting the Program!")
+print("")
+
+copying_PDF_files
+convert_all_to_CSV
+moving_CSV_files
--- scanner.py
+++ scanner.py
@@ -30,8 +30,8 @@ def Copy_Files(source_dir, file_name, dest_dir):
         print(file_name + "is already in the folder")
         return
 
-#scanning the files within the dir
-def Scan_Files(source_dir):
+#scanning the files within the dir and RETURNS an Array based on file type
+def Scan_Files(source_dir, file_type):
 
     names_array = []
 
@@ -45,19 +45,22 @@ def Scan_Files(source_dir):
         for entry in itr :
             file_name = entry.name
 
-            if file_name.endswith(".pdf"):
+            if file_name.endswith(file_type):
                 #print(file_name)
                 
                 names_array.append(file_name)
                 #Copy_Files(source_dir, file_name, dest_dir)
-    
+
+    #returns an array of file names
     return names_array
    
 #============================================================
 #Starting Hash
+print("Using Scanner.py!")
+print("")
 hash = {}
 
-#using Scan_Files runs through the main directory
+#using Scan_Files runs through the main directory (Finance_INFO)
 obj = os.scandir(main_source_dir)
 
 with os.scandir(main_source_dir) as itr:
@@ -66,8 +69,12 @@ with os.scandir(main_source_dir) as itr:
         if entry.name.startswith("."):
             continue
 
+        #dir includes: Tuition / Rent / Bank 
         dir_name = main_source_dir + entry.name
 
-        hash[dir_name] = Scan_Files(dir_name)
+        #using hash to store every pdf files in each dir 
+        #KEY: dir_name
+        #VALUE: array of filenames
+        hash[dir_name] = Scan_Files(dir_name, ".pdf")
          
     #print(hash)
